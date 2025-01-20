1510d68b71639a90faff7c29367dd3e3744e0736
Jonathan Johansson <jonathan.johansson.3@consultant.volvo.com>
2025-01-20 17:08:58 +0100

    clean up

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -28,18 +28,7 @@ lspconfig.lua_ls.setup({
 
 lspconfig.eslint.setup({
 	capabilities = capabilities,
-	--- ...
-	-- on_attach = function(client, bufnr)
-	-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 		buffer = bufnr,
-	-- 		command = "EslintFixAll",
-	-- 	})
-	-- end,
 })
--- lspconfig.eslint.setup({
---
--- 	capabilities = capabilities,
--- })
 
 cmp.setup({
 	snippet = {
@@ -100,28 +89,3 @@ autocmd("BufWritePost", {
 	group = "__formatter__",
 	command = ":FormatWrite",
 })
-
--- -- keeps track of current `tabline` and `statusline`, so we can restore it after closing telescope
--- local temp_showtabline
--- local temp_laststatus
---
--- function _G.global_telescope_find_pre()
--- 	print("jaacacjajcaj")
--- 	temp_showtabline = vim.o.showtabline
--- 	temp_laststatus = vim.o.laststatus
--- 	vim.o.showtabline = 0
--- 	vim.o.laststatus = 0
--- end
---
--- function _G.global_telescope_leave_prompt()
--- 	vim.o.laststatus = temp_laststatus
--- 	vim.o.showtabline = temp_showtabline
--- end
---
--- vim.cmd([[
---   augroup MyAutocmds
---     autocmd!
---     autocmd User TelescopeFindPre lua global_telescope_find_pre()
---     autocmd FileType TelescopePrompt autocmd BufLeave <buffer> lua global_telescope_leave_prompt()
---   augroup END
--- ]])
